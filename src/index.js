cf59364452ff5a8a0167d1f1b5ff160604269090
RT <rentao0524@163.com>
2022-10-21 17:18:21 +0800

    create task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const puppeteer = require("puppeteer");
+const { pageUrl, userInfo, assigneeUserName } = require('./constants')
+
+const summary_description = 'summary demo'
 
 puppeteer
   .launch({
@@ -7,9 +10,42 @@ puppeteer
   })
   .then(async (browser) => {
     await login(browser)
-    await sleep(3000)
-    await gotoJiraPage(browser)
+    await sleep(30000)
+    const page = await gotoJiraPage(browser)
+    await createTask(page)
   });
+
+async function createTask(page) {
+  await page.waitForSelector('#create_link')
+  const createBtn = await page.$('#create_link')
+  await createBtn.click()
+
+  await page.waitForSelector('#issuetype-field')
+  // issue type
+  const issueTypeSelect = await page.$('#issuetype-field')
+  await issueTypeSelect.click()
+
+  // await page.waitForSelector('.aui-list-item-li-task')
+  // await page.evaluate(() => {
+  //   console.log(document.querySelector('.aui-list-item-li-task'))
+  // })
+
+  const summaryInput = await page.$('#summary')
+  await summaryInput.type(summary_description)
+
+  const assigneeFieldInput = await page.$('#assignee-field')
+  await assigneeFieldInput.type(assigneeUserName)
+  await page.waitForSelector('#assignee-suggestions')
+  await page.evaluate(() => {
+    document.querySelector('#suggestions').childNodes[0].click()
+  })
+}
+
+async function gotoJiraPage(browser) {
+  const page2 = await browser.newPage();
+  await page2.goto(pageUrl);
+  return page2
+}
 
 function sleep(time) {
   return new Promise((resolve) => {
@@ -21,10 +57,11 @@ function sleep(time) {
 
 async function login(browser) {
   const page = await browser.newPage();
-  await page.goto("https://did-sso.bba-app.biz/b2bloginhtml/index.html?bmctx=86A43AC017E77E0242FC614C41AD4B856005A7D1605CAA05305FE1BB92A1FA3F&contextType=external&username=string&password=secure_string&challenge_url=https%3A%2F%2Fdid-sso.bba-app.biz%2Fb2bloginhtml%2Findex.html&targetid=did_bmw&request_id=6173111550440630786&authn_try_count=0&locale=zh_CN&resource_url=https%253A%252F%252Fdid-sso.bba-app.biz%252Fserveroidclogin%253Fscope%253Dopenid%252520email%252520profile%252520phone%252520address%252520offline_access%252520bbaAtcUid%2526state%253DoCvQGFY99sls5dhtBw6TqwTgQkRyEgd9YqM8uzlqiCw.dKspcMKVVm4.security-admin-console%2526response_type%253Dcode%2526client_id%253D55ajetbxzikdk%2526redirect_uri%253Dhttps%253A%252F%252Fatc.bmw-brilliance.cn%252Fauth%252Frealms%252Fmaster%252Fbroker%252Foidc%252Fendpoint%2526nonce%253DfQeyl9e-MRO2dx3lqvMavg");
-}
+  await page.goto(pageUrl);
 
-async function gotoJiraPage(browser) {
-  const page2 = await browser.newPage();
-  await page2.goto("https://atc.bmw-brilliance.cn/jira/secure/Dashboard.jspa");
+  const userInfoInput = await page.$('.b_login_input')
+  await userInfoInput.type(userInfo.u)
+  const passewordInput = await page.$('#password')
+  await passewordInput.type(userInfo.password)
+
 }
